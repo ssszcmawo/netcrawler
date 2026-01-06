c459d63d3ba36b514b9b379a1010e0f1009ee8ad
ssszcmawo <belan2470@gmail.com>
2026-01-06 21:02:25 +0100

    deleted last_error

---
+++ include/HttpsClient.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <curl/curl.h>
+
+class HttpsClient
+{
+public:
+    HttpsClient();
+   ~HttpsClient();
+
+    std::string get_request(std::string& url);
+
+private: 
+    static size_t write_callback(void* contents,std::size_t size,std::size_t nmemb,void* userp);
+
+private: 
+    CURL* curl;
+
+    long timeout_seconds;
+    long connection_timeout_seconds;
+
+    std::string user_agent;
+    std::string proxy;
+
+    bool follow_redirects;
+    int max_redirects;
+
+    char error_buffer[CURL_ERROR_SIZE];
+};
