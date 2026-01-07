a9fe6c06215b997a9162e31645644403240c6dc2
ssszcmawo <belan2470@gmail.com>
2026-01-07 02:08:15 +0100

    add parse function and export to csv file

--- include/HtmlParser.hpp
+++ include/HtmlParser.hpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <libxml/HTMLparser.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <fstream>
 
 struct Product
 {
@@ -13,7 +17,15 @@ struct Product
 class HtmlParser
 {
 public:
-    HtmlParser(const std::string& html){}
+    HtmlParser(const std::string& html)
+    {
+        parse(html);
+    }
+
+    ~HtmlParser()
+    {
+        
+    }
 
     const std::vector<Product>& get_products() const { return products; }
 
@@ -22,6 +34,10 @@ public:
 
 private:
     void parse(const std::string& html);
+    void export_to_csv(const std::string& filename);
 
     std::vector<Product> products;
+    htmlDocPtr doc;
+    xmlXPathContextPtr context;
+    xmlXPathObjectPtr html_elements;
 };
