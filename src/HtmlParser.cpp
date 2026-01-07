87ab43b0113b2d3facaac70e59fcaf71ab3c6a56
ssszcmawo <belan2470@gmail.com>
2026-01-07 21:01:39 +0100

    fixed parse function

--- src/HtmlParser.cpp
+++ src/HtmlParser.cpp
@@ -1,23 +1,5 @@
 #include "HtmlParser.hpp"
 
-HtmlParser::~HtmlParser()
-{
-   if(html_elements) {
-        xmlXPathFreeObject(html_elements);
-        html_elements = nullptr;
-    }
-
-    if(context) {
-        xmlXPathFreeContext(context);
-        context = nullptr;
-    }
-
-    if(doc) {
-        xmlFreeDoc(doc);
-        doc = nullptr;
-    }
-}
-
 void HtmlParser::parse(const std::string& html)
 {
     doc = htmlReadMemory(html.c_str(), html.size(), nullptr, nullptr, HTML_PARSE_NOERROR);
@@ -52,33 +34,35 @@ void HtmlParser::parse(const std::string& html)
         if(!product_node) continue;
 
         auto get_text_from_xpath = [&](const char* xpath_expr) -> std::string {
-            xmlXPathObjectPtr obj = xmlXPathNodeEval(product_node, (xmlChar*)xpath_expr, context);
-            std::string result;
-            if(obj && obj->nodesetval && obj->nodesetval->nodeNr > 0) {
-                xmlNodePtr node = obj->nodesetval->nodeTab[0];
-                if(node) {
-                    xmlChar* content = nullptr;
-                    if(node->type == XML_ELEMENT_NODE) {
-                        content = xmlNodeGetContent(node);
-                    } else {
-                        content = xmlGetProp(node, (xmlChar*)"href");
-                    }
-                    if(content) {
-                        result = reinterpret_cast<char*>(content);
-                        xmlFree(content);
-                    }
-                }
+        context->node = product_node;
+
+        xmlXPathObjectPtr obj =
+        xmlXPathEvalExpression((xmlChar*)xpath_expr, context);
+
+        std::string result;
+
+        if (obj && obj->type == XPATH_NODESET &&
+            obj->nodesetval && obj->nodesetval->nodeNr > 0)
+        {
+            xmlNodePtr node = obj->nodesetval->nodeTab[0];
+            xmlChar* content = xmlNodeGetContent(node);
+            if (content) {
+            result = (char*)content;
+            xmlFree(content);
             }
-            if(obj) xmlXPathFreeObject(obj);
-            return result;
-        };
+        }
+
+        if (obj) xmlXPathFreeObject(obj);
+        return result;
+};
+
 
         std::string url   = get_text_from_xpath(".//a/@href");
-        std::string image = get_text_from_xpath(".//a/img/@src");
+        std::string image = get_text_from_xpath(".//img/@src");
         std::string name  = get_text_from_xpath(".//a/h2");
         std::string price = get_text_from_xpath(".//a/span");
 
-        Product product = {url, image, name, price};
+        Product product = {url, name, image, price};
         products.push_back(product);
     }
 
@@ -94,16 +78,17 @@ void HtmlParser::export_to_csv(const std::string& filename)
 {
    std::ofstream csv_file(filename);
 
-   csv_file << "url,image,name,price" << "\n";
+   csv_file << "url,name,image,price" << "\n";
 
    for(int i = 0; i < products.size(); ++i)
    {
       Product p = products.at(i);
 
-      std::string csv_record = p.url + "," + p.image + "," + p.name + "," + p.price;
+      std::string csv_record = p.url + "," + p.name + "," + p.image + "," + p.price;
 
       csv_file << csv_record << "\n";
    }
 
    csv_file.close();
 }
+
