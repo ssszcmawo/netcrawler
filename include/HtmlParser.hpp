efd5fe21b008c43ac2fda86bb149647b04d0fb3d
ssszcmawo <belan2470@gmail.com>
2026-01-09 19:54:30 +0100

    replace all functions which are not related to HTML parsing in HtmlParser Class,add wrappers for xml variables

--- include/HtmlParser.hpp
+++ include/HtmlParser.hpp
@@ -1,11 +1,13 @@
+#pragma once
+#include <filesystem>
+#include <fstream>
 #include <iostream>
-#include <vector>
 #include <libxml/HTMLparser.h>
 #include <libxml/parser.h>
 #include <libxml/xpath.h>
-#include <fstream>
-#include <filesystem>
 #include <optional>
+#include <string_view>
+#include <vector>
 
 struct Product
 {
@@ -13,31 +15,97 @@ struct Product
     std::string name;
     std::string image;
     std::string price;
-
 };
+
+namespace Utils
+{
+extern std::vector<Product> products;
+
+std::string escape_csv(const std::string &field);
+std::optional<double> parse_price(const std::string_view str);
+std::vector<Product> read_csv(const std::string &filename);
+void export_to_csv(const std::string &filename);
+std::optional<Product> find_product_by_name(const std::string &filename,
+    const std::string &name);
+void filter_csv_by_price(const std::string &filename, double min, double max);
+const std::vector<Product> &get_products();
+} // namespace Utils
 
 class HtmlParser
 {
-public:
-    HtmlParser(const std::string& html)
+  public:
+    HtmlParser(const std::string &html)
     {
         parse(html);
     }
+    ~HtmlParser() {};
 
-    ~HtmlParser(){};
+  private:
+    void parse(const std::string &html);
 
-    const std::vector<Product>& get_products() const { return products; }
+    struct XmlDocWrapper
+    {
+        xmlDocPtr doc;
+        XmlDocWrapper(xmlDocPtr d) :
+            doc(d)
+        {
+        }
+        ~XmlDocWrapper()
+        {
+            if (doc)
+                xmlFreeDoc(doc);
+        }
+        operator xmlDocPtr() const
+        {
+            return doc;
+        }
+        xmlDocPtr operator->() const
+        {
+            return doc;
+        }
+    };
 
-    std::optional<Product> find_product_by_name(const std::string& filename,const std::string& name);
-    void filter_csv_by_price(const std::string& filename,double min,double max);
+    struct XmlXPathContextWrapper
+    {
+        xmlXPathContextPtr ctx;
+        XmlXPathContextWrapper(xmlXPathContextPtr c) :
+            ctx(c)
+        {
+        }
+        ~XmlXPathContextWrapper()
+        {
+            if (ctx)
+                xmlXPathFreeContext(ctx);
+        }
+        operator xmlXPathContextPtr() const
+        {
+            return ctx;
+        }
+        xmlXPathContextPtr operator->() const
+        {
+            return ctx;
+        }
+    };
 
-private:
-    void parse(const std::string& html);
-    void export_to_csv(const std::string& filename);
-    std::vector<Product> read_csv(const std::string& filename); 
-
-    std::vector<Product> products;
-    htmlDocPtr doc;
-    xmlXPathContextPtr context;
-    xmlXPathObjectPtr html_elements;
+    struct XmlXPathObjectWrapper
+    {
+        xmlXPathObjectPtr obj;
+        XmlXPathObjectWrapper(xmlXPathObjectPtr o) :
+            obj(o)
+        {
+        }
+        ~XmlXPathObjectWrapper()
+        {
+            if (obj)
+                xmlXPathFreeObject(obj);
+        }
+        operator xmlXPathObjectPtr() const
+        {
+            return obj;
+        }
+        xmlXPathObjectPtr operator->() const
+        {
+            return obj;
+        }
+    };
 };
