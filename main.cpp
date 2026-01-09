8d89cb0b9100dad587c338372466f85260517f0f
ssszcmawo <belan2470@gmail.com>
2026-01-09 20:06:09 +0100

    update main.cpp for new Utils-based architecture

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include "HttpsClient.hpp"
 #include "HtmlParser.hpp"
-
+#include "HttpsClient.hpp"
+#include <curl/curl.h>
+#include <fstream>
+#include <iostream>
 
 int main()
 {
@@ -16,29 +17,26 @@ int main()
     std::string filename = "page.html";
 
     std::ofstream out(filename);
-
-    if(out.is_open())
+    if (out.is_open())
     {
         out << document;
         out.close();
-
         std::cout << "page.html saved\n";
-    }else{
+    }
+    else
+    {
         std::cerr << "Could not open page.html\n";
-    } 
+    }
 
     HtmlParser parser(document);
 
-    parser.filter_csv_by_price("products.csv",40,50);
+    Utils::filter_csv_by_price("products.csv", 40, 50);
 
-    auto prod = parser.find_product_by_name("products.csv", "Ana Running Short");
-    if(prod)
-     std::cout << prod->name << " "<< prod->price << "\n";
-    else{
+    auto prod = Utils::find_product_by_name("products.csv", "Ana Running Short");
+    if (prod)
+        std::cout << prod->name << " " << prod->price << "\n";
+    else
         std::cout << "error\n";
-    }
-
-
 
     curl_global_cleanup();
 
